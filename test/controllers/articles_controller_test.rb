388719102314ef6c5901d4722871362dbcb60dfd
SHERRY <swasieko@gmail.com>
2017-04-05 19:00:14 +0000

    Initial commit

---
+++ test/controllers/articles_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ArticlesControllerTest < ActionController::TestCase
+  
+  test 'should get articles index' do
+    get :index
+    assert_response :success
+  end
+ 
+end
